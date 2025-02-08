7c20e205befd7181de31c3c5c71c22e579ca8062
mac <a0979552111@gmail.com>
2025-02-08 11:11:11 +0000

    docker can flash SD

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,7 @@ PASSWD=0                  # Password (0 is likely a placeholder, update as neede
 docker_images_name="rpi4_img:latest"  # Name and tag for the Docker image (lowercase only)
 docker_container_name="rpi4_image"    # Name for the Docker container (lowercase only)
 docker_workspace_path="/home/$USER/RPI4"  # Workspace path inside the container
+device_path="/dev/sda"
 ##########################################################
 # Run the Docker container
 # Map the current directory to the workspace path inside the container
@@ -13,5 +14,6 @@ docker_workspace_path="/home/$USER/RPI4"  # Workspace path inside the container
 
 docker run -id -v `pwd`:$docker_workspace_path \
 --network host \
+--device $device_path:$device_path \
 -w $docker_workspace_path --user $USER \
 --name $docker_container_name $docker_images_name
